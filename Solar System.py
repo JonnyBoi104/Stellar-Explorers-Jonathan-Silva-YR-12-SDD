aaf341751e36b4f29c3e8965dcd3884eb45a8e81
JonnyBoi104 <jonathanja.silva1@gmail.com>
2024-05-13 21:43:18 +1000

    Update Solar System.py

--- Solar System.py
+++ Solar System.py
@@ -23,8 +23,8 @@ y = (screen_height / 2) - (app_height / 2)
 root.geometry(f'{app_width}x{app_height}+{int(x)}+{int(y)}')
 
 
-font_style = ("Cambria", 35)
-
+title_font = ("Cambria", 35)
+theme_font = ("Cambria", 28)
 
 #Back Buttons and stuff--------------------------------------------------------------------------#
 
@@ -61,7 +61,7 @@ def select_view():
     select_view_frame = CTkFrame(root)
     select_view_frame.pack(expand=True, fill=BOTH)
 
-    select_view_label = CTkLabel(select_view_frame, text="Would You like to view?", text_color= "Black", font=font_style)
+    select_view_label = CTkLabel(select_view_frame, text="Would You like to view?", text_color= "Black", font=title_font)
     select_view_label.place(relx=0.5, rely=0.13, anchor="center")
 
     sequential_order_btn = CTkButton(select_view_frame, text="Sequential Order (The Sun)", width=100, height=80, command=sequential_view) 
@@ -78,7 +78,7 @@ def sequential_view():
     sun_view_frame = CTkFrame(root)
     sun_view_frame.pack(expand=True, fill=BOTH) 
 
-    sun_title_label = CTkLabel(sun_view_frame, text="SUN", text_color= "Black", font=font_style)
+    sun_title_label = CTkLabel(sun_view_frame, text="SUN", text_color= "Black", font=title_font)
     sun_title_label.place(relx=0.5, rely=0.13, anchor="center") 
 
     back_btn = CTkButton(sun_view_frame, text="Back", text_color="Black", width=50, command=back_to_sequential_view)
@@ -91,7 +91,7 @@ def orbit_view():
     orbit_view_frame = CTkFrame(root) 
     orbit_view_frame.pack(expand=True, fill=BOTH)  
 
-    orbit_view_label = CTkLabel(orbit_view_frame, text="Where would you like to go?", text_color= "Black", font=font_style)
+    orbit_view_label = CTkLabel(orbit_view_frame, text="Where would you like to go?", text_color= "Black", font=title_font)
     orbit_view_label.place(relx=0.5, rely=0.13, anchor="center") 
 
     back_btn = CTkButton(orbit_view_frame, text="Back", text_color="Black", width=50, command=back_to_sequential_view_2)
@@ -112,8 +112,17 @@ def options_view():
     back_btn = CTkButton(options_view_frame, text="Back", text_color="Black", width=50, command=back_to_main_menu_2)
     back_btn.place(relx=0.09, rely=0.1, anchor="center") 
 
+   #Themes
+    theme_label = CTkLabel(options_view_frame, text="Themes", text_color="black", font=theme_font) 
+    theme_label.place(relx=0.2, rely=0.225) 
+    themes = ["Dark", "Light", "Midnight", "Navy", "Biege", "Botanical"]
+    theme_combo = CTkComboBox(options_view_frame, values=themes, command=theme_select) 
+    theme_combo.place(relx=0.18, rely=0.3)
 
+def theme_select(choice): 
+    pass
 
+   #Zoom
 #--------------------------------------------------------------------------------------------------------------------------------------#
 
 
@@ -129,7 +138,7 @@ def quiz_view():
 
 main_frame = CTkFrame(root)
 main_frame.pack(expand=True, fil=BOTH)
-title_label = CTkLabel(main_frame, text="Stellar Explorers", text_color= "Black", font=font_style)
+title_label = CTkLabel(main_frame, text="Stellar Explorers", text_color= "Black", font=title_font)
 title_label.place(relx=0.5, rely=0.13, anchor="center")
 start_btn = CTkButton(main_frame, text = "Start", command=select_view, width=200, height=35) 
 start_btn.pack(pady=200)
